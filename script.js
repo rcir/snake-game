0e7ecc8a9ab084af15fb1eb8e59bb3e25692e628
rcir <121425382+rcir@users.noreply.github.com>
2023-01-03 10:10:06 +0100

    Add a function to generate a div
    Added a function to create a div, give it a class name and append it
    to the game container. It receives a class name as a parameter and
    adds it to the div, making the function usable for both food and snake
    part elements.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const container = document.querySelector("#game-container");
+
+function generateDiv(className){
+    let div = document.createElement("div");
+    div.classList.add(className);
+    container.appendChild(div);
+}
