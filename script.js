2013797225dcd58b054231e128be7bcc7fe3757a
rcir <121425382+rcir@users.noreply.github.com>
2023-01-04 09:21:29 +0100

    Add a function to place a div on the grid
    Added a function to place a div on the grid. It receives a div with X
    and Y coordinates as parameters.

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function generateDiv(className){
     div.classList.add(className);
     container.appendChild(div);
 }
+function placeDiv(div, x, y){
+    div.style.gridColumnStart = x;
+    div.style.gridRowStart = y;
+}
