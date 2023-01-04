6ec8dbb6308f526d9e43c3afdd81df6edffaefb9
rcir <121425382+rcir@users.noreply.github.com>
2023-01-04 09:29:31 +0100

    Make the generateDiv function return the element
    Added a return statement to the generateDiv function to allow for the
    use of the created element in the placeDiv function.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function generateDiv(className){
     let div = document.createElement("div");
     div.classList.add(className);
     container.appendChild(div);
+    return div;
 }
 function placeDiv(div, x, y){
     div.style.gridColumnStart = x;
